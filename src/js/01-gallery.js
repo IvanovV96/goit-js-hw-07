0d67fd6263822da4877d8ae277e73e8b3f66f762
Vitaliy Ivanov <dotaivanov@gmail.com>
2022-08-07 10:28:03 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery')
+function renderGallery(images) {
+    const markupArr = []
+    images.forEach(img => {
+        const template = `<div class="gallery__item">
+        <a class="gallery__link" href="${img.original}">
+          <img
+            class="gallery__image"
+            src="${img.preview}"
+            data-source="${img.original}"
+            alt="${img.description}"
+          />
+        </a>
+      </div>`
+      markupArr.push(template)
+    })
+    gallery.insertAdjacentHTML('beforeend', markupArr.join(''))
+}
 
-console.log(galleryItems);
+renderGallery(galleryItems)
+
+gallery.addEventListener('click', (e) => {
+    e.preventDefault()
+    if(e.target.nodeName !== 'IMG') {
+        return
+    }
+   const instance = basicLightbox.create(`
+   <img src="${e.target.dataset.source}" width="800" height="600">
+   `)
+    instance.show()
+    function closeEscape(e) {
+        if(e.code === 'Escape') {
+            instance.close()
+        }
+    }
+    document.addEventListener('keydown', closeEscape)
+})
+
+
+
+
+
+
+
