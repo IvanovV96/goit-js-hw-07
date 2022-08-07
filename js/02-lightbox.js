89ce7c0fd9c6dc4e858c887d1fea0333e20fbe3e
Vitaliy Ivanov <dotaivanov@gmail.com>
2022-08-07 10:54:58 +0300

    fix

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,9 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-var lightbox = new SimpleLightbox('.gallery a', { 
-    captionsData : 'alt',
-    captionDelay: 250
-});
+
 
 const gallery = document.querySelector('.gallery')
 
@@ -19,4 +16,9 @@ function renderGallery(images) {
     gallery.insertAdjacentHTML('beforeend', markupArr.join(''))
 }
 renderGallery(galleryItems)
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData : 'alt',
+    captionDelay: 250
+});
 
